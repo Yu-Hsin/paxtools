fbebced49afea2aef0a2896d6ebdbb1df9985c45
Yu-Hsin Kuo <yuhsink@cs.cmu.edu>
2015-03-17 22:51:37 -0400

    updated

--- src/main/java/Pxtool.java
+++ src/main/java/Pxtool.java
@@ -2,6 +2,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Set;
 
+import org.biopax.paxtools.impl.level3.BindingFeatureImpl;
+import org.biopax.paxtools.impl.level3.CellularLocationVocabularyImpl;
+import org.biopax.paxtools.impl.level3.FragmentFeatureImpl;
+import org.biopax.paxtools.impl.level3.ModificationFeatureImpl;
+import org.biopax.paxtools.impl.level3.SequenceIntervalImpl;
+import org.biopax.paxtools.impl.level3.SequenceLocationImpl;
+import org.biopax.paxtools.impl.level3.SequenceSiteImpl;
 import org.biopax.paxtools.io.BioPAXIOHandler;
 import org.biopax.paxtools.io.SimpleIOHandler;
 import org.biopax.paxtools.model.Model;
@@ -12,10 +19,9 @@ import org.biopax.paxtools.model.level3.EntityFeature;
 import org.biopax.paxtools.model.level3.PhysicalEntity;
 import org.biopax.paxtools.model.level3.Control;
 import org.biopax.paxtools.model.level3.TemplateReaction;
+import org.biopax.paxtools.model.level3.TemplateReactionRegulation;
 
 public class Pxtool {
-
-
 
     public static void main(String[] args) {
 	// Load a sample test BioPAX File via Simple IO Handler
@@ -24,26 +30,28 @@ public class Pxtool {
 	    fin = new FileInputStream("data/testmodel.owl");
 	    BioPAXIOHandler handler = new SimpleIOHandler();
 	    Model model = handler.convertFromOWL(fin); // read a model
+	    
+	    // get all the conversion from the model 
 	    for (Conversion conversion : model.getObjects(Conversion.class)) {
-		Set<PhysicalEntity> left = conversion.getLeft();
-		Set<PhysicalEntity> right = conversion.getRight();
-		Set<Control> ctrl = conversion.getControlledOf();
+		Set<PhysicalEntity> left = conversion.getLeft(); // get left part 
+		Set<PhysicalEntity> right = conversion.getRight(); // get right part
+		Set<Control> ctrl = conversion.getControlledOf(); // get controllers
 
 		String l = traverse(left);
 		String r = traverse(right);
+		
 		String control = traverseCtrl(ctrl);
 		if (control.length() != 0) 
 		    System.out.println(l + " -> " + r + " Controller: " + control);
 		else
 		    System.out.println(l + " -> " + r);
-
+		
 	    }
+	    /*
+	    System.out.println(model.getObjects(TemplateReactionRegulation.class));
+	    System.out.println(model.getObjects(TemplateReaction.class));
+	     */
 	    
-	    
-	    for (TemplateReaction tr : model.getObjects(TemplateReaction.class)) {
-		Set <PhysicalEntity> pe = tr.getProduct();
-	    }
-
 	} catch (FileNotFoundException e) {
 	    e.printStackTrace();
 	}
@@ -55,14 +63,17 @@ public class Pxtool {
 	for (PhysicalEntity tmp : pe) {
 	    ans += traversePE(tmp) + " + ";
 	}
-	return ans.length() == 0 ? "" : ans.substring(0, ans.length() - 3);
+	return ans.length() == 0 ? "" : ans.substring(0, ans.length() - 3); // has to remove the trailing " + "
     }
 
     public static String traversePE(PhysicalEntity pe) {
 
-	String str1 = "(", str2 = "[", str3 = "";
-
-	if (pe instanceof Complex) {
+	// "()" for complex
+	// "[]" for PE generic
+	String str1 = "(", str2 = "[", str3 = ""; 
+	
+	// check if it is a complex - if so using getParticipants 
+	if (pe instanceof Complex) { 
 	    Set<PhysicalEntity> peSet = ((Complex) pe).getComponent();
 	    for (PhysicalEntity tmpPE : peSet) {
 		str1 += traversePE(tmpPE) + ", ";
@@ -70,9 +81,10 @@ public class Pxtool {
 
 	}
 	str1 = str1.length() == 1 ? "" : str1.substring(0, str1.length() - 2);
-	if (str1.length() != 0)
+	if (str1.length() != 0) // complete the parenthesis
 	    str1 += ")";
 
+	// check if it is a PE generic  
 	if (pe.getMemberPhysicalEntity().size() != 0) {
 	    Set<PhysicalEntity> peSet = pe.getMemberPhysicalEntity();
 	    for (PhysicalEntity tmpPE : peSet)
@@ -83,10 +95,10 @@ public class Pxtool {
 	if (str2.length() != 0)
 	    str2 += "]";
 
+	// not a complex and PE generic
 	if (!(pe instanceof Complex)
 		&& pe.getMemberPhysicalEntity().size() == 0) {
 
-	    
 	    String name = "";
 	    Set<String> nameSet = pe.getName();
 	    for (String tmp : nameSet) {
@@ -94,12 +106,39 @@ public class Pxtool {
 		break;
 	    }
 	    str3 += name + ", ";
-
+	    str3 = str3.length() == 0 ? "" : str3.substring(0, str3.length() - 2);
+	    
 	    Set <EntityFeature> ef = pe.getFeature();
-	    System.out.println(ef);
+	    if (pe.getCellularLocation() != null)
+		System.out.println("subcelluar: " + pe.getCellularLocation().getTerm());
+	    
+	    for (EntityFeature tmpEf : ef) {
+		if (tmpEf instanceof ModificationFeatureImpl) {
+		    //System.out.println("modification type: " + ((ModificationFeatureImpl) tmpEf).getModificationType().getTerm());
+		    for (String tmpN : ((ModificationFeatureImpl) tmpEf).getModificationType().getTerm()) {
+			str3 += " (Type: " + tmpN + ")";
+			break;
+		    }
+		    
+		}
+		if (tmpEf instanceof FragmentFeatureImpl)
+		    System.out.println("fragment feature: " + ((FragmentFeatureImpl)tmpEf));
+		
+		/*if (tmpEf instanceof BindingFeatureImpl)
+		    System.out.println(((BindingFeatureImpl) tmpEf).getBindsTo());*/
+		
+		if (tmpEf.getFeatureLocation() != null) {
+		    System.out.println("AAAAAAAA");
+		    if (tmpEf.getFeatureLocation() instanceof SequenceSiteImpl)
+			System.out.println("Location: " + tmpEf.getFeatureLocation().toString());
+		    if (tmpEf.getFeatureLocation() instanceof SequenceIntervalImpl) {
+			System.out.println("Location start: " + ((SequenceIntervalImpl)tmpEf.getFeatureLocation()).getSequenceIntervalBegin().toString());
+			System.out.println("Location end: " + ((SequenceIntervalImpl)tmpEf.getFeatureLocation()).getSequenceIntervalEnd().toString());
+		    }
+		}
+	    }
+	    
 	}
-	str3 = str3.length() == 0 ? "" : str3.substring(0, str3.length() - 2);
-
 	String ans = "";
 	if (str1.length() != 0)
 	    ans += str1;
